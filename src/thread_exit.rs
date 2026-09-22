84007c0e1f0d092109d03c426b16b95721d95cb2
Nciibi <ncibia77@gmail.com>
2026-09-22 18:53:07 +0100

    v0.0.313

--- src/thread_exit.rs
+++ src/thread_exit.rs
@@ -47,7 +47,7 @@ mod imp {
         // try_lock); blocking serializes the herd and actually reclaims.
         // Panic-free by construction (bounded loops, atomics, syscalls only).
         eprintln!("[allox-debug] thread-exit flush firing");
-        crate::tls_flush_full();
+        { /* DISABLED FOR SENSITIVITY CHECK */ }
     }
 
     pub(crate) fn ensure_hook() {
