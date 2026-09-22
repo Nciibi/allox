5c1fba6563568c2590ecd246c0554ee57de9ab39
Nciibi <ncibia77@gmail.com>
2026-09-22 19:06:07 +0100

    v0.0.341

--- benches/alloc.rs
+++ benches/alloc.rs
@@ -190,6 +190,7 @@ fn run<A: GlobalAlloc + Sync + ?Sized>(alloc: &'static A, wl: &Workload, seconds
         Kind::Standard => run_standard(alloc, wl, seconds),
         Kind::ProdCons => run_prodcons(alloc, wl, seconds),
         Kind::SpawnChurn => run_spawn_churn(alloc, wl, seconds),
+        Kind::SpawnEmpty => run_spawn_empty(wl, seconds),
     }
 }
 
