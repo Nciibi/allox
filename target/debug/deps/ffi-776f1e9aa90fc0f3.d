3252fc48084fdf2d8d78a6c37fc60902317d947c
Nciibi <ncibia77@gmail.com>
2026-09-23 16:36:27 +0100

    v0.0.575

--- target/debug/deps/ffi-776f1e9aa90fc0f3.d
+++
@@ -1,9 +0,0 @@
-D:\projects\allocator\target\debug\deps\ffi-776f1e9aa90fc0f3.d: tests\ffi.rs Cargo.toml
-
-D:\projects\allocator\target\debug\deps\libffi-776f1e9aa90fc0f3.rmeta: tests\ffi.rs Cargo.toml
-
-tests\ffi.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__
-# env-dep:CLIPPY_CONF_DIR
