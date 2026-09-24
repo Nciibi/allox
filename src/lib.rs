20f2bb5198647cb0ae51e622b76bc3fadce39bcb
Nciibi <ncibia77@gmail.com>
2026-09-24 19:56:27 +0100

    v0.0.942

--- src/lib.rs
+++ src/lib.rs
@@ -1613,18 +1613,6 @@ pub fn __debug_arena_detail() -> (u64, u64) {
 }
 
 #[doc(hidden)]
-pub fn __debug_arena_coalesce_stats() -> (u64, u64, u64) {
-    #[cfg(all(unix, feature = "std"))]
-    {
-        crate::arena::coalesce_stats()
-    }
-    #[cfg(not(all(unix, feature = "std")))]
-    {
-        (0, 0, 0)
-    }
-}
-
-#[doc(hidden)]
 pub fn __debug_arena_hole_stats() -> (u64, u64, u64, u64) {
     #[cfg(all(unix, feature = "std"))]
     {
