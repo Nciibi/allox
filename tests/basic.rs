2e5dd5c4e324f4f5b8313c05b01145d426f2c727
Nciibi <ncibia77@gmail.com>
2026-09-24 12:41:26 +0100

    v0.0.800

--- tests/basic.rs
+++ tests/basic.rs
@@ -294,7 +294,9 @@ fn forged_large_header_does_not_steal_medium_free() {
         (*header).base = second.sub(128);
         (*header).requested = 28000;
         (*header).next = core::ptr::null_mut();
+        eprintln!("second={:x} page={:x} before={}", second as usize, second as usize & !65535, usable_size(second));
         *((second as usize & !65535) as *mut u8).cast::<u64>() = 0xA110_CCA7_E5A1_1E5D;
+        eprintln!("after={}", usable_size(second));
         assert!(usable_size(second) >= 28000);
         for p in blocks {
             free(p);
