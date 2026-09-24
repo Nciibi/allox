865750f6ce858a5585301b015061da8da88ac606
Nciibi <ncibia77@gmail.com>
2026-09-24 13:16:41 +0100

    v0.0.844

--- src/arena.rs
+++ src/arena.rs
@@ -834,7 +834,7 @@ mod tests {
         let (tail, tail_fresh) = unsafe { a.commit(1) };
         assert!(!tail.is_null() && !tail_fresh);
         assert_eq!(tail, unsafe { base.add(pages * ARENA_ALIGN) });
-        assert_eq!(a.granule_stats(), (1, (4 * ARENA_GRANULE_SIZE) as u64));
+        assert_eq!(a.granule_stats(), (1, (3 * ARENA_GRANULE_SIZE) as u64));
         unsafe { a.release(tail, 1) };
     }
 
