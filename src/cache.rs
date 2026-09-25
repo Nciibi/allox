d8e0cf549483c2661305f46e29c87e891a4b3377
Nciibi <ncibia77@gmail.com>
2026-09-25 15:10:39 +0100

    v0.0.1211

--- src/cache.rs
+++ src/cache.rs
@@ -547,10 +547,10 @@ impl ThreadCache {
     #[inline]
     fn drift_gate_open(&self) -> bool {
         if self.tier_cached_bytes == 0 {
-            return self.cached_bytes > thread_cache_budget() / DRIFT_GATE_DIV;
+            return self.cached_bytes > self.budget / DRIFT_GATE_DIV;
         }
-        self.small_cached_bytes() > thread_cache_budget() / DRIFT_GATE_DIV
-            || self.tier_cached_bytes > tier_cache_budget() / DRIFT_GATE_DIV
+        self.small_cached_bytes() > self.budget / DRIFT_GATE_DIV
+            || self.tier_cached_bytes > self.budget.saturating_mul(2) / DRIFT_GATE_DIV
     }
 
     #[inline]
