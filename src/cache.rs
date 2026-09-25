5fb7b4207b453cf185366a9c50f7382c8a84560e
Nciibi <ncibia77@gmail.com>
2026-09-25 10:29:19 +0100

    v0.0.1088

--- src/cache.rs
+++ src/cache.rs
@@ -281,10 +281,6 @@ impl ThreadCache {
         }
     }
 
-    pub(crate) fn debug_bytes(&self) -> (usize, usize, usize) {
-        (self.cached_bytes, self.actual_tier_bytes(), self.tier_cached_bytes)
-    }
-
     /// Publish accumulated telemetry deltas to the global atomics.
     #[cfg(feature = "telemetry")]
     fn publish(&mut self) {
